e1c92ca52837fa93543da7491272a2416aa9cbaa
Dylan <dylan.mattiola@etu.univ-lyon1.fr>
2015-06-15 20:41:56 +0200

    presque end

--- test/test/model/mapmanager/mapManagerJUnitTest.java
+++ test/test/model/mapmanager/mapManagerJUnitTest.java
@@ -5,8 +5,6 @@ import model.algorithme.Dijkstra;
 import model.graph.*;
 import model.mapmanager.MapManager;
 import model.robot.*;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
 
